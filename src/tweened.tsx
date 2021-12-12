558b67da348b4160e7db8a80365c3ec407399a19
inokawa <48897392+inokawa@users.noreply.github.com>
2021-12-12 23:37:45 +0900

    Add onTweenStart / onTweenEnd prop

--- src/tweened.tsx
+++ src/tweened.tsx
@@ -44,133 +44,143 @@ export const tweened = <P extends object>(
   render: TweenRender<P>,
   opts: TweenOpts = {}
 ) => {
-  return memo((props: P) => {
-    const refresh = useForceRefresh();
-    const visible = useRef(true);
-    const refs = useRef<React.RefObject<any>[]>([]);
-    const tweens = useRef<TweenTarget[][]>(null!);
-    const prevNode = useRef<React.ReactElement | null>(null);
+  return memo(
+    (props: P & { onTweenStart?: () => void; onTweenEnd?: () => void }) => {
+      const refresh = useForceRefresh();
+      const visible = useRef(true);
+      const refs = useRef<React.RefObject<any>[]>([]);
+      const tweens = useRef<TweenTarget[][]>(null!);
+      const prevNode = useRef<React.ReactElement | null>(null);
 
-    const transitionState = useContext(TransitionStateContext);
-    const transitionKey = useContext(TransitionKeyContext);
-    const transitionRemove = useContext(TransitionRemoveContext);
+      const transitionState = useContext(TransitionStateContext);
+      const transitionKey = useContext(TransitionKeyContext);
+      const transitionRemove = useContext(TransitionRemoveContext);
 
-    if (transitionState !== "exit") {
-      visible.current = true;
-    }
-
-    const register = <T extends string | number>(...args: [T] | [T, T]): T => {
-      if (args.length === 2) {
-        return new TweenableProp(args[1], args[0], transitionState) as any;
+      if (transitionState !== "exit") {
+        visible.current = true;
       }
-      return new TweenableProp(args[0], null, transitionState) as any;
-    };
 
-    let refIndex = 0;
-    tweens.current = [];
+      const register = <T extends string | number>(
+        ...args: [T] | [T, T]
+      ): T => {
+        if (args.length === 2) {
+          return new TweenableProp(args[1], args[0], transitionState) as any;
+        }
+        return new TweenableProp(args[0], null, transitionState) as any;
+      };
 
-    const makeNodeRenderable = (n: React.ReactElement): React.ReactElement => {
-      const nodeTweens: TweenTarget[] = [];
-      const children: React.ReactNode[] = [];
-      const tweenProps = {} as { [key: string]: any };
-      if (typeof n.type === "string") {
-        Object.keys(n.props).forEach((k) => {
-          const p = n.props[k];
-          if (k === "children") {
-            Children.forEach(p as React.ReactNode, (n) => {
-              if (isValidElement(n)) {
-                children.push(makeNodeRenderable(n));
-                return;
-              }
-              children.push(n);
-            });
-            return;
-          } else if (k === "style") {
-            Object.keys(p[k]).forEach((sk) => {
-              const sp = p[k][sk];
-              if (sp instanceof TweenableProp) {
-                nodeTweens.push({ type: "style", k: sk, p: sp });
-                const prevProp = prevNode.current?.props[k]?.[sk];
-                if (prevProp != null) {
-                  tweenProps[k][sk] = prevProp;
-                } else {
-                  tweenProps[k][sk] = sp.to;
+      let refIndex = 0;
+      tweens.current = [];
+
+      const makeNodeRenderable = (
+        n: React.ReactElement
+      ): React.ReactElement => {
+        const nodeTweens: TweenTarget[] = [];
+        const children: React.ReactNode[] = [];
+        const tweenProps = {} as { [key: string]: any };
+        if (typeof n.type === "string") {
+          Object.keys(n.props).forEach((k) => {
+            const p = n.props[k];
+            if (k === "children") {
+              Children.forEach(p as React.ReactNode, (n) => {
+                if (isValidElement(n)) {
+                  children.push(makeNodeRenderable(n));
+                  return;
                 }
-              }
-            });
-            return;
-          }
-          if (p instanceof TweenableProp) {
-            nodeTweens.push({ type: "attr", k, p });
-            const prevProp = prevNode.current?.props[k];
-            if (prevProp != null) {
-              tweenProps[k] = prevProp;
-            } else {
-              tweenProps[k] = p.to;
+                children.push(n);
+              });
+              return;
+            } else if (k === "style") {
+              Object.keys(p[k]).forEach((sk) => {
+                const sp = p[k][sk];
+                if (sp instanceof TweenableProp) {
+                  nodeTweens.push({ type: "style", k: sk, p: sp });
+                  const prevProp = prevNode.current?.props[k]?.[sk];
+                  if (prevProp != null) {
+                    tweenProps[k][sk] = prevProp;
+                  } else {
+                    tweenProps[k][sk] = sp.to;
+                  }
+                }
+              });
+              return;
             }
-          }
-        });
-      }
+            if (p instanceof TweenableProp) {
+              nodeTweens.push({ type: "attr", k, p });
+              const prevProp = prevNode.current?.props[k];
+              if (prevProp != null) {
+                tweenProps[k] = prevProp;
+              } else {
+                tweenProps[k] = p.to;
+              }
+            }
+          });
+        }
 
-      if (nodeTweens.length === 0) {
+        if (nodeTweens.length === 0) {
+          return cloneElement(
+            n,
+            undefined,
+            children.length === 0 ? undefined : children
+          );
+        }
+
+        tweens.current.push(nodeTweens);
+        tweenProps.ref =
+          refs.current[refIndex] || (refs.current[refIndex] = createRef());
+        refIndex++;
         return cloneElement(
           n,
-          undefined,
+          tweenProps,
           children.length === 0 ? undefined : children
         );
-      }
+      };
 
-      tweens.current.push(nodeTweens);
-      tweenProps.ref =
-        refs.current[refIndex] || (refs.current[refIndex] = createRef());
-      refIndex++;
-      return cloneElement(
-        n,
-        tweenProps,
-        children.length === 0 ? undefined : children
-      );
-    };
+      const renderableNode = visible.current
+        ? makeNodeRenderable(
+            render(props, { state: transitionState, tween: register })
+          )
+        : null;
 
-    const renderableNode = visible.current
-      ? makeNodeRenderable(
-          render(props, { state: transitionState, tween: register })
-        )
-      : null;
+      useLayoutEffect(() => {
+        if (!visible.current) return;
 
-    useLayoutEffect(() => {
-      if (!visible.current) return;
+        props.onTweenStart?.();
+        const queues: TweenObject[] = [];
+        refs.current.forEach((ref, i) => {
+          const t = startTween(
+            ref.current,
+            tweens.current[i],
+            opts.duration,
+            opts.ease
+          );
+          queues.push(t);
+        });
 
-      const queues: TweenObject[] = [];
-      refs.current.forEach((ref, i) => {
-        const t = startTween(
-          ref.current,
-          tweens.current[i],
-          opts.duration,
-          opts.ease
-        );
-        queues.push(t);
+        if (transitionState === "exit" || props.onTweenEnd) {
+          (async () => {
+            try {
+              await Promise.all(queues.map((q) => q.end()));
+              props.onTweenEnd?.();
+              if (transitionState === "exit") {
+                visible.current = false;
+                transitionRemove(transitionKey);
+                refresh();
+              }
+            } catch (e) {
+              // NOP
+            }
+          })();
+        }
+
+        return () => {
+          queues.forEach((t) => t.interrupt());
+        };
       });
 
-      if (transitionState === "exit") {
-        (async () => {
-          try {
-            await Promise.all(queues.map((q) => q.end()));
-            visible.current = false;
-            transitionRemove(transitionKey);
-            refresh();
-          } catch (e) {
-            // NOP
-          }
-        })();
-      }
+      prevNode.current = renderableNode;
 
-      return () => {
-        queues.forEach((t) => t.interrupt());
-      };
-    });
-
-    prevNode.current = renderableNode;
-
-    return renderableNode;
-  });
+      return renderableNode;
+    }
+  );
 };
