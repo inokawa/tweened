aeef554a05bb22384ed8abd2674a135c8e628118
inokawa <48897392+inokawa@users.noreply.github.com>
2022-02-14 01:04:35 +0900

    Remove unused hook

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,27 +1,6 @@
-import { useState, useCallback, useRef, useEffect } from "react";
+import { useState, useCallback } from "react";
 
 export const useForceRefresh = (): [number, () => void] => {
   const [count, setState] = useState(0);
   return [count, useCallback(() => setState((p) => p + 1), [])];
-};
-
-export const useResettableRef = <T, D>(
-  initialValue: T,
-  dep: D
-): [T, (value: T) => void] => {
-  const value = useRef(initialValue);
-  const prevDep = useRef<D>(null!);
-  let currentValue = dep !== prevDep.current ? initialValue : value.current;
-
-  useEffect(() => {
-    value.current = currentValue;
-    prevDep.current = dep;
-  });
-
-  return [
-    currentValue,
-    (v: T) => {
-      value.current = v;
-    },
-  ];
 };
